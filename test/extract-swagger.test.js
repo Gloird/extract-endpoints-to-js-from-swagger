ba36944a531cda640d41c35e52a256c335d9e851
Nicolas Dupuis <nicolas86200@gmail.com>
2021-06-30 15:43:16 +0200

    tu

--- test/extract-swagger.test.js
+++ test/extract-swagger.test.js
@@ -1,32 +1,5 @@
 'use strict';
 
-const extractEndpoints = require('../lib/extract-endpoints');
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
-const assert = require('assertive');
-
 describe('Extract endpoints', () => {
-  let doc, resp;
-  before(() => {
-    try {
-      doc = yaml.safeLoad(
-        fs.readFileSync(path.join(__dirname, '/fixtures/petstore.yml'), 'utf-8')
-      );
-    } catch (e) {
-      throw e;
-    }
-
-    resp = extractEndpoints(doc, ['/pet/findByStatus']);
-  });
-  it('matches stored schema', () => {
-    const expected = yaml.safeLoad(
-      fs.readFileSync(
-        path.join(__dirname, './fixtures/petstore-findByStatus.yml'),
-        'utf-8'
-      )
-    );
-    assert.deepEqual(expected.paths, resp.limitPaths);
-    assert.deepEqual(expected.definitions, resp.limitDefinitions);
-  });
+  
 });
